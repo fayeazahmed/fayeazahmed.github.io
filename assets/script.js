88dc52ad0ad30fabf74bcd923e8637f58f2d8683
fayeazahmed <r10.ahmed@yahoo.com>
2021-01-26 21:30:50 +0600

    transformed to a single page look

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,96 @@
-//NAV SCRIPT
+//CAROUSEL
+const TECH_BTNS = document.querySelectorAll(".technologies__buttons");
+const TECH_CONTAINER = document.querySelector(".technologies__container");
+const TECH_PAGES = document.querySelectorAll(".portfolio__project");
+const NAV_BTNS = document.querySelectorAll(".nav .btn");
+const SECTIONS = document.querySelectorAll(".spa");
+const CAROUSEL = document.querySelectorAll(".carousel-controller i");
+const DP = document.querySelector(".dp");
+let TECH_STACKED = true;
+let nextPage = 0;
 
-const HOME_BTN = document.getElementById("nav-home");
-const MAIN = document.getElementById("main");
-HOME_BTN.addEventListener("click", () => (MAIN.scrollTop = 0));
+function changeCarousel(buttonPosition) {
+  nextPage = buttonPosition + 1;
+  TECH_BTNS.forEach((btn, i) => {
+    if (i === buttonPosition) btn.classList.toggle("buttons--activated");
+    else btn.classList.remove("buttons--activated");
+  });
 
-//SCROLL SCRIPT
+  TECH_PAGES.forEach((div, i) => {
+    if (i === buttonPosition) div.classList.toggle("--activated");
+    else div.classList.remove("--activated");
+  });
 
-const SCROLLBAR = document.getElementById("scroll-bar");
-const MAIN_HEIGHT = MAIN.scrollHeight;
-const BIO = document.getElementById("bio");
+  checkIfStacked(buttonPosition);
+  checkIfNextPageExists();
+}
 
-MAIN.addEventListener("scroll", () => {
-  const scrolledHeight = MAIN.scrollTop;
-  let scrollBarHeight = (scrolledHeight / MAIN_HEIGHT) * 100;
-  if (isElementInViewport(BIO)) {
-    scrollBarHeight = 100;
+function navigate(position) {
+  NAV_BTNS.forEach((btn, i) => {
+    if (i === position) btn.classList.add("--activated");
+    else btn.classList.remove("--activated");
+  });
+
+  if (position === 0) {
+    DP.classList.remove("dp--transformed");
+    changeCarousel(69);
+  } else {
+    DP.classList.add("dp--transformed");
   }
-  SCROLLBAR.style.height = scrollBarHeight + "%";
-  SCROLLBAR.style.top = scrolledHeight + "px";
+
+  SECTIONS.forEach((div, i) => {
+    if (i === position) div.classList.add("page--activated");
+    else div.classList.remove("page--activated");
+  });
+}
+
+TECH_BTNS.forEach((button, position) => {
+  button.addEventListener("click", () => changeCarousel(position));
 });
 
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+NAV_BTNS.forEach((button, position) => {
+  button.addEventListener("click", () => navigate(position));
+});
+
+CAROUSEL.forEach((button, position) => {
+  button.addEventListener("click", () => {
+    if (position === 1 && nextPage < TECH_PAGES.length) {
+      changeCarousel(nextPage);
+    } else if (position === 0 && nextPage > 0) {
+      changeCarousel(nextPage - 2);
+    }
+  });
+});
+
+function checkIfStacked(position) {
+  if (TECH_PAGES[position]) {
+    TECH_STACKED = TECH_PAGES[position].classList.contains("--activated")
+      ? false
+      : true;
+  } else {
+    TECH_STACKED = true;
+  }
+  TECH_STACKED
+    ? TECH_CONTAINER.classList.remove("stacked")
+    : TECH_CONTAINER.classList.add("stacked");
 }
+
+function checkIfNextPageExists() {
+  if (nextPage === TECH_PAGES.length) {
+    CAROUSEL[1].style.visibility = "hidden";
+  } else if (nextPage === 0) {
+    CAROUSEL[0].style.visibility = "hidden";
+  } else {
+    CAROUSEL.forEach((button) => (button.style.visibility = "visible"));
+  }
+}
+
+// MAIN.addEventListener("scroll", () => {
+//   const scrolledHeight = MAIN.scrollTop;
+//   let scrollBarHeight = (scrolledHeight / MAIN_HEIGHT) * 100;
+//   if (isElementInViewport(BIO)) {
+//     scrollBarHeight = 100;
+//   }
+//   SCROLLBAR.style.height = scrollBarHeight + "%";
+//   SCROLLBAR.style.top = scrolledHeight + "px";
+// })
