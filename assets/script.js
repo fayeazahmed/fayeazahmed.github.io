19c95a81d73110fb1a072f0cc4a944af7b0fed03
fayeazahmed <r10.ahmed@yahoo.com>
2021-03-20 00:16:03 +0600

    changed design

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,7 @@
-//CAROUSEL
-const TECH_BTNS = document.querySelectorAll(".technologies__buttons");
-const TECH_CONTAINER = document.querySelector(".technologies__container");
-const TECH_PAGES = document.querySelectorAll(".portfolio__project");
 const NAV_BTNS = document.querySelectorAll(".nav .btn");
 const SECTIONS = document.querySelectorAll(".spa");
-const CAROUSEL = document.querySelectorAll(".carousel-controller i");
+const PROJECT_SECTIONS = document.querySelectorAll(".projects__sections")
 const DP = document.querySelector(".dp");
-let TECH_STACKED = true;
-let nextPage = 0;
-
-function changeCarousel(buttonPosition) {
-  nextPage = buttonPosition + 1;
-  TECH_BTNS.forEach((btn, i) => {
-    if (i === buttonPosition) btn.classList.toggle("buttons--activated");
-    else btn.classList.remove("buttons--activated");
-  });
-
-  TECH_PAGES.forEach((div, i) => {
-    if (i === buttonPosition) div.classList.toggle("--activated");
-    else div.classList.remove("--activated");
-  });
-
-  checkIfStacked(buttonPosition);
-  checkIfNextPageExists();
-}
 
 function navigate(position) {
   NAV_BTNS.forEach((btn, i) => {
@@ -33,7 +11,7 @@ function navigate(position) {
 
   if (position === 0) {
     DP.classList.remove("dp--transformed");
-    changeCarousel(69);
+    expandSection(-1)
   } else {
     DP.classList.add("dp--transformed");
   }
@@ -44,53 +22,18 @@ function navigate(position) {
   });
 }
 
-TECH_BTNS.forEach((button, position) => {
-  button.addEventListener("click", () => changeCarousel(position));
-});
-
 NAV_BTNS.forEach((button, position) => {
   button.addEventListener("click", () => navigate(position));
 });
 
-CAROUSEL.forEach((button, position) => {
-  button.addEventListener("click", () => {
-    if (position === 1 && nextPage < TECH_PAGES.length) {
-      changeCarousel(nextPage);
-    } else if (position === 0 && nextPage > 0) {
-      changeCarousel(nextPage - 2);
-    }
+function expandSection(position) {
+  PROJECT_SECTIONS.forEach((div, i) => {
+    if (i === position) div.classList.toggle("projects__sections--activated");
+    else div.classList.remove("projects__sections--activated");
   });
+}
+
+PROJECT_SECTIONS.forEach((div, position) => {
+  div.querySelector("h4").addEventListener("click", () => expandSection(position));
 });
-
-function checkIfStacked(position) {
-  if (TECH_PAGES[position]) {
-    TECH_STACKED = TECH_PAGES[position].classList.contains("--activated")
-      ? false
-      : true;
-  } else {
-    TECH_STACKED = true;
-  }
-  TECH_STACKED
-    ? TECH_CONTAINER.classList.remove("stacked")
-    : TECH_CONTAINER.classList.add("stacked");
-}
-
-function checkIfNextPageExists() {
-  if (nextPage === TECH_PAGES.length) {
-    CAROUSEL[1].style.visibility = "hidden";
-  } else if (nextPage === 0) {
-    CAROUSEL[0].style.visibility = "hidden";
-  } else {
-    CAROUSEL.forEach((button) => (button.style.visibility = "visible"));
-  }
-}
-
-// MAIN.addEventListener("scroll", () => {
-//   const scrolledHeight = MAIN.scrollTop;
-//   let scrollBarHeight = (scrolledHeight / MAIN_HEIGHT) * 100;
-//   if (isElementInViewport(BIO)) {
-//     scrollBarHeight = 100;
-//   }
-//   SCROLLBAR.style.height = scrollBarHeight + "%";
-//   SCROLLBAR.style.top = scrolledHeight + "px";
-// })
+navigate(1)
